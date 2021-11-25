04e3256ad7b683be217b8b3a30f651b65013447c
Lamduy0807 <19521421@gm.uit.edu.vn>
2021-11-25 10:59:43 +0700

    add business logic to Profile and setting profile screen

--- src/pages/Profile/ProfileScreen.js
+++ src/pages/Profile/ProfileScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
     Dimensions,
     ImageBackground,
@@ -25,7 +25,66 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Feather from 'react-native-vector-icons/Feather'
 import { Avatar } from 'react-native-paper';
-const ProfileScreen = ({navigator})=>{
+import { AuthContext } from '../../context/context';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+const ProfileScreen = ({navigation})=>{
+
+    const { signOut } = React.useContext(AuthContext);
+    const [data, setData] = useState({
+        name: "Loading...",
+        id: "",
+        avt: "http://127.0.0.1:8000/media/logo-uit.png",
+        phonenum: "loading...",
+        token: "" 
+    })
+    useEffect(() => {
+        const getData = async () => {
+            try {
+              const value = await AsyncStorage.getItem('userToken')
+              const valueid = await AsyncStorage.getItem('id')
+              console.log("value: ", value)
+
+              getInfo(valueid, value);
+              if(value !== null) {
+              }
+            } catch(e) {
+                alert("no data")
+            }
+          }
+        const getInfo = async (id, token) => {
+            console.log("token: ", 'Bearer '+ token)
+            await fetch ('http://10.0.2.2:8000/user/'+ id + '/',
+        {
+            method:'GET',
+            headers:{
+                'Authorization': 'Bearer '+ token,
+                'Content-Type': 'application/json'
+            },
+        }).then(response=>{
+            if(response.status==200)
+            {
+                response.json().then(d=>{
+                    console.log("name: ", d.name)
+                    setData({
+                        ...data,
+                        name: d.name,
+                        phonenum: d.phone,
+                        avt: d.avt
+                    })
+                })
+            }
+        })
+        .then(res => {
+            //console.log("reponse :", res); 
+           }).catch(error => {
+            console.error("eroor",error);
+            return { name: "network error", description: "" };
+          });
+        }
+        getData();
+    }, [])
+
+
     return(
         <View style={{flex:1}}>
             <ScrollView style={{ backgroundColor:"#EEEEEE"}}>
@@ -33,12 +92,12 @@ const ProfileScreen = ({navigator})=>{
                 <View style={{ marginLeft:scale(widthofscreen/15),flexDirection:"row"}}>
                     
                     <View style={{justifyContent:"center",}}>
-                        <Avatar.Image size={scale(widthofscreen/6.5)} source={require("../../assets/image/star.jpg")}/>
+                        <Avatar.Image style={{backgroundColor:"#FFF"}} size={scale(widthofscreen/6.5)}  source={{uri : data.avt.replace("127.0.0.1","10.0.2.2")}}/>
                     </View>
                       
                     <View style={{flexDirection:"column", justifyContent:"center", marginLeft: scale(10)}}>
-                        <Text style={{fontSize:scale(20),fontWeight:"bold", color:"#FFF"}}>Your Name</Text>
-                        <Text style={{fontSize:scale(15),fontWeight:"600", color:"#FFF"}}>09000001</Text>
+                        <Text style={{fontSize:scale(20),fontWeight:"bold", color:"#FFF"}}>{data.name}</Text>
+                        <Text style={{fontSize:scale(15),fontWeight:"600", color:"#FFF"}}>{data.phonenum}</Text>
                     </View> 
                 </View>
                 
@@ -97,7 +156,7 @@ const ProfileScreen = ({navigator})=>{
             </View>
         
             <View style={styles.setting}>
-                <TouchableOpacity style={styles.itemsetting}>
+                <TouchableOpacity style={styles.itemsetting} onPress={()=>{navigation.navigate('SettingAccount')}}>
                     <MaterialCommunityIcons name="account-outline" size={scale(24)} style={{marginLeft: scale(10)}} color="#231F20"/>
                     <Text style={{fontSize: scale(17), fontWeight: '300', marginLeft: scale(10)}}>Cài đặt tài khoản</Text>
                 </TouchableOpacity>
@@ -113,7 +172,8 @@ const ProfileScreen = ({navigator})=>{
                 </TouchableOpacity>
             </View>
             <View style={styles.setting}>
-                <TouchableOpacity style={styles.itemsetting}>
+                <TouchableOpacity style={styles.itemsetting}
+                onPress={()=>{signOut()}}>
                     <MaterialIcons name="logout" size={scale(24)} style={{marginLeft: scale(10)}} color="#231F20"/>
                     <Text style={{fontSize: scale(17), fontWeight: '300', marginLeft: scale(10)}}>Đăng xuất</Text>
                 </TouchableOpacity>
