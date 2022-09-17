f521347598003cb942feb9f23e232e140aa3291d
thanhnga0104 <19521880@gm.uit.edu.vn>
2022-09-17 13:22:24 +0700

    Update styled

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -2,4 +2,12 @@ export const AppColor = {
   transparent: '#00000000',
   transparent1: '#000000AA',
   white0: '#FFFFFF',
+  white1: '#ECF3F9',
+  white2: '#F2F2F2',
+  black0: '#000000',
+  green0: '#14A445',
+  green1: '#039B52',
+  green2: '#006C25',
+  grey1: '#B6C7D1',
+  blue1: '#05375a',
 };
